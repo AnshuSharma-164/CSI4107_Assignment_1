e5cc2e55c9cd14de083351a938282c6618304047
TheoVanderburgt <theo.a.vdb@gmail.com>
2021-02-10 14:52:35 -0500

    added comments

--- A1.py
+++ A1.py
@@ -8,32 +8,30 @@ import re
 stopwords = "StopWords.txt" #list stop words
 tweets = "Trec_microblog11.txt" #txt of the original tweets
 
+tknzr = TweetTokenizer()# create tokenizer
 
-tknzr = TweetTokenizer()
+stopWordsList = pd.read_csv(stopwords, sep="\n", header=None, error_bad_lines=False) #stopwords dataset
+data = pd.read_csv(tweets, sep="\t", header=None, error_bad_lines=False) #tweets dataset
 
-
-stopWordsList = pd.read_csv(stopwords, sep="\n", header=None, error_bad_lines=False)
-data = pd.read_csv(tweets, sep="\t", header=None, error_bad_lines=False)
-
-stopWordsList.columns = ["words"]
-data.columns = ["tweetID", "tweet"]
-data.insert(2,"tokens", 1,allow_duplicates=True)
-tweetList = data.loc[:,"tweet"]
-stops = stopWordsList.loc[:,"words"]
+stopWordsList.columns = ["words"]#set column name
+data.columns = ["tweetID", "tweet"]#set column names
+data.insert(2,"tokens", 1,allow_duplicates=True)#create token row
+tweetList = data.loc[:,"tweet"]#create token array
+stops = stopWordsList.loc[:,"words"]#create stopword array
 
 
 tokenArray = []
 tweetTokensCopy = []
 for tweet in tweetList:
-    tweetTokens = tknzr.tokenize(tweet)
+    tweetTokens = tknzr.tokenize(tweet) # tokenize tweets
     tweetTokensCopy = []
     for word in tweetTokens:
-        word = word.lower()
-        word = re.sub("\W+","a",word)
-        word = re.sub("[0-9]+","a",word)
-        if word not in stopWordsList.values:
+        word = word.lower() #set all tweet tokens lowercase
+        word = re.sub("\W+","a",word) #remove non-alphabet characters
+        word = re.sub("[0-9]+","a",word) #remove numbers
+        if word not in stopWordsList.values: # only add to output non-stopwords
             tweetTokensCopy.append(word)
-    tokenArray.append(tweetTokensCopy)#TODO this does not recive updated list
+    tokenArray.append(tweetTokensCopy) #add tweet tokens to output
 # print(tokenArray)
 
 
