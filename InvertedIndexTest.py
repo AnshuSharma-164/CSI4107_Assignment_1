0a2761d681acf2d2059463cb0ea67d8bbe9a0117
Anshu Sharma <ashar164@uottawa.ca>
2021-02-10 04:38:19 +0300

    Expanded Tests

--- InvertedIndexTest.py
+++ InvertedIndexTest.py
@@ -1,11 +1,28 @@
 import InvertedIndex
 
 test = InvertedIndex.InvertedIndex()
-test.insertToken("new" , 1)
-test.insertToken("york", 1)
-test.insertToken("times" , 1)
-test.insertToken("new" , 2)
-test.insertToken("york", 2)
-test.insertToken("post" , 2)
-test.insertTokenList(["los", "angeles", "times"] , 3)
+
+test.insertToken("new" , "d1")
+test.insertToken("york", "d1")
+test.insertToken("times" , "d1")
+test.insertToken("new" , "d2")
+test.insertToken("york", "d2")
+test.insertToken("post" , "d2")
+test.insertTokenList(["los", "angeles", "times"] , "d3")
 print(test)
+
+x = []
+x.append(test.idf("angeles"))
+x.append(test.idf("los"))
+x.append(test.idf("times"))
+x.append(test.idf("times"))
+x.append(test.idf("times"))
+x.append(test.idf("york"))
+print(x)
+
+y = test.normalizeTf("times" , "d1")
+print(y)
+
+z = test.documentWeight("times", "d1")
+print(z)
+
